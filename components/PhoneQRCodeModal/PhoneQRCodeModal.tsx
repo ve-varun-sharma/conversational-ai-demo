ceb6df06b5515dece5680d33f1d226961f72a816
Varun Sharma <varun_sharma@live.co.uk>
2024-02-06 12:37:04 -0500

    feat: complete alpha demp

---
+++ components/PhoneQRCodeModal/PhoneQRCodeModal.tsx
@@ -0,0 +1,50 @@
+"use client";
+import React from "react";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+  Image,
+} from "@nextui-org/react";
+
+export default function App() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  return (
+    <>
+      <Button onPress={onOpen} variant="shadow" color="success">
+        Call our A.I. now
+      </Button>
+      <Modal backdrop={"blur"} isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Call us!
+              </ModalHeader>
+              <ModalBody>
+                <p>
+                  Scan the QR code with your phone to call us or directly @
+                  +14158497544
+                </p>
+                <Image width={300} alt="NextUI hero Image" src="/qr-code.png" />
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                {/* <Button color="primary" onPress={onClose}>
+                  Action
+                </Button> */}
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
